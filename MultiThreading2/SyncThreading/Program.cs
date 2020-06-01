082d238632f44c53ce64e918483d64b1beee40dd
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2020-06-01 22:42:00 +0200

    Syn and Priority

---
+++ MultiThreading2/SyncThreading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+namespace SyncThreading
+{
+    class MyThread
+    {
+        public void Thread1()
+        {
+            lock (this)
+            {
+                Thread.Sleep(200);
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyThread mt = new MyThread();
+            Thread t1 = new Thread(new ThreadStart(mt.Thread1));
+            Thread t2 = new Thread(new ThreadStart(mt.Thread1));
+            Thread t3 = new Thread(new ThreadStart(mt.Thread1));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+    }
+}
