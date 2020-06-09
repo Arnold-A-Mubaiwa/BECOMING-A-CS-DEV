3106f467e7650588f6d0e163d2f1e8ca57d041b8
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2020-06-09 23:30:28 +0200

    Database creation and connection

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcMovie.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MvcMovie
 {
@@ -16,6 +18,8 @@ namespace MvcMovie
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
             services.AddControllersWithViews ();
+             services.AddDbContext<MvcMovieContext>(options =>
+        options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
