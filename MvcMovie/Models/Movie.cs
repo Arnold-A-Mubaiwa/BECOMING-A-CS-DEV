d0b229fa5d9ba5d1efde0f98137b8750894aab2e
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2020-06-10 21:39:45 +0200

    NEW FIELD
    Signed-off-by: Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 namespace MvcMovie.Models
 {
@@ -6,10 +7,15 @@ namespace MvcMovie.Models
     {
         public int Id{get;set;}
         public string Title{get;set;}
+
+        [Display(Name ="Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate{get;set;}
         public string Genre {get;set;}
+
+        [Column(TypeName="decimal(18, 2)")]
         public decimal Price {get; set;}
+        public string Rating {get; set;}
 
     }
 }
