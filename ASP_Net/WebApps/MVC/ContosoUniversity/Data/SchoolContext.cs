e4c31b2c1e9f84eec665b30a00ae092f2e1abf97
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2020-06-27 16:08:05 +0200

    College attendace using mvc

---
+++ ASP_Net/WebApps/MVC/ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Data{
+    public class SchoolContext: DbContext{
+        public SchoolContext(DbContextOptions<SchoolContext> options):
+                    base(options){
+
+                    }
+        
+        public DbSet<Student> Students{get;set;}
+        public DbSet<Course> Courses{get;set;}
+        public DbSet<Enrollment> Enrollments{get;set;} 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+        }
+    }
+}
