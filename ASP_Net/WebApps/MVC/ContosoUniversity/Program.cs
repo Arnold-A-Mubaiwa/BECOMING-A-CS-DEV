2fca85c0dcf7a070a36afcacd1158286e3533ed6
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2020-06-28 22:32:36 +0200

    Creating student pages

--- ASP_Net/WebApps/MVC/ContosoUniversity/Program.cs
+++ ASP_Net/WebApps/MVC/ContosoUniversity/Program.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ContosoUniversity.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ContosoUniversity
 {
@@ -13,7 +11,21 @@ namespace ContosoUniversity
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope()){
+                var services = scope.ServiceProvider;
+
+                try{
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitilizer.Initilize(context);
+                }
+                catch(Exception ex){
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            host.Run();
+            // CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
