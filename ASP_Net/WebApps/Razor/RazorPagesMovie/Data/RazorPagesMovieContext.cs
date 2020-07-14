c7b49e03de7e4781130cac9da1d9c08b4f1c77bd
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2020-07-14 23:51:33 +0200

    New app

---
+++ ASP_Net/WebApps/Razor/RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Data{
+    public class RazorPagesMovieContext : DbContext{
+        public RazorPagesMovieContext(
+            DbContextOptions<RazorPagesMovieContext> options
+        ): base(options){
+
+        }
+        public DbSet<Movie> Movies{get;set;}
+    }
+}
